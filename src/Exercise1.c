d44fd28af91b7785ce3f0cf99ba2159add957123
dinhvuduc <80943015+dinhvuduc761@users.noreply.github.com>
2021-03-26 14:47:02 +0700

    Update Exercise1.c

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -16,7 +16,7 @@ void Ex1(int arr[], int n){
 	//your codes here
     int i ,j;
 	int sym =1;
-	for (i=0, j=n-1;(a<= floor((n-1)/2))&&(j>=ceil((n-1)/2)); i++,j--){
+	for (i=0, j=n-1;(i<= floor((n-1)/2))&&(j>=ceil((n-1)/2)); i++,j--){
 		if(arr[j]!=arr[i])
 		sym=0;
 	}
